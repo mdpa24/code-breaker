a7bf1643e454e84bb88c58eefc3bd2cbad8ce360
mdpa24 <mdpa24@outlook.com>
2023-03-23 23:22:44 -0600

    Se cambió el orden en  que se muestran los asteriscos

--- codigo.js
+++ codigo.js
@@ -140,15 +140,13 @@ switch(guiones) {
         
         case 0: console.log(numero1+numero2+numero3+numero4+" -");
         break;
-        case 1: console.log(numero1+numero2+numero3+numero4+" -*");
+        case 1: console.log(numero1+numero2+numero3+numero4+" *-");
         break;
-        case 2: console.log(numero1+numero2+numero3+numero4+" -**");
+        case 2: console.log(numero1+numero2+numero3+numero4+" **-");
         break;
-        case 3: console.log(numero1+numero2+numero3+numero4+" -***");
+        case 3: console.log(numero1+numero2+numero3+numero4+" ***-");
         break;
-        case 4: console.log(numero1+numero2+numero3+numero4+" -****");
-        break;
-    }
+        
     break;
 
     case 2:
@@ -156,9 +154,9 @@ switch(guiones) {
 
         case 0: console.log(numero1+numero2+numero3+numero4+" --");
         break;
-        case 1: console.log(numero1+numero2+numero3+numero4+" --*");
+        case 1: console.log(numero1+numero2+numero3+numero4+" *--");
         break;
-        case 2: console.log(numero1+numero2+numero3+numero4+" --**");
+        case 2: console.log(numero1+numero2+numero3+numero4+" **--");
         break;
     }
     break;
@@ -168,7 +166,7 @@ switch(guiones) {
 
         case 0: console.log(numero1+numero2+numero3+numero4+" ---");
         break;
-        case 1: console.log(numero1+numero2+numero3+numero4+" ---*");
+        case 1: console.log(numero1+numero2+numero3+numero4+" *---");
         break;
     }
     break;
